45f59f2280b26ad9c12aaf2d66fc390eb0c09ba2
Sreehari Nagesh <sreeharinagesh@gmail.com>
2023-05-07 03:54:11 +0530

    :/

--- api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class IsReadOnly(permissions.BasePermission):
-    '''Allow read only permission for anonymous users'''
-    def has_permission(self, request, view):
-        if self.request.method in permissions.SAFE_METHODS:
-            return True
-        return False
